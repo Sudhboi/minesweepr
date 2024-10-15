107f8ce52e8441cc2aadb6828c19073dc7b3a669
Sudhboi <sudhboiii@gmail.com>
2024-10-15 21:57:33 +0530

    Added Reference Pictures

--- pyautoguitest.py
+++ pyautoguitest.py
@@ -1,20 +1,19 @@
 import pyautogui as pg
 
-refx, refy = pg.locateCenterOnScreen('resetbutton.png') #1636 420
+refx, refy = pg.locateCenterOnScreen('resetbutton.png', confidence = 0.5) #1636 420
+refx, refy = int(refx), int(refy)
 pos0x , pos0y = refx - 121, refy + 34 #1515 454
 
 def return_coordinates(x, y):
     return pos0x + (16 * x), pos0y + (16 * y)
 
 def click_tile(x, y):
-    pg.moveTo(return_coordinates(x, y))
+    rx, ry = return_coordinates(x, y)
+    pg.moveTo(rx, ry, 0.5)
     pg.click()
 
 def reset():
     pg.moveTo(refx, refy)
-    pg.click()
+    pg.doubleClick()
     print(refx, refy)
 
-
-reset()
-click_tile(7, 7)
